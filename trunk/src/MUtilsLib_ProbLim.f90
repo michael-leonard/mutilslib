696a0e1aed38953292bd815322c1467dcce42843
michael.leonard.au@gmail.com <michael.leonard.au@gmail.com@aa557fbc-4e6d-11de-9ad6-1127a62b9fcd>
2010-09-04 04:18:21 +0000

    Fixed public interface.

--- trunk/src/MUtilsLib_ProbLim.f90
+++ trunk/src/MUtilsLib_ProbLim.f90
@@ -37,7 +37,6 @@ end interface calcProblim
 ! Procedural Availability
 private
 public :: problimType,CalcProblim,hpsort,stanResidualsProb,pValResidualsProb
-public :: hpsortInd, isSameR4, isSameR8
 public :: hpsortInd
 contains
 !****************************************************************************
